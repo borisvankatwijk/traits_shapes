61ed5cb0e8c6edff48cf2c28c72e079b9756ea84
Boris van Katwijk <borisvankatwijk@gmail.com>
2023-11-20 21:04:07 +0100

    Restructure; test with Result type and assert_eq!

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,171 @@
-trait TwoDimensional {
-    fn area(&self) -> f32;
+use std::io::Error;
+
+#[derive(Debug)]
+enum Shape {
+    // Two dimensional
+    Square { side: f32 },
+    Rectangle { length: f32, width: f32 },
+    Circle { radius: f32 },
+    // Three dimensional
+    Cube { width: f32, height: f32, depth: f32 },
+    Sphere { radius: f32 },
 }
 
-trait ThreeDimensional {
-    fn volume(&self) -> f32;
-}
+impl Shape {
+    fn new_square() -> Shape {
+        Shape::Square { side: 0.0 }
+    }
 
-struct Square {
-    side: f32,
-}
+    fn new_rectangle() -> Shape {
+        Shape::Rectangle { length: 0.0, width: 0.0 }
+    }
 
-struct Rectangle {
-    length: f32,
-    width: f32,
-}
+    fn new_circle() -> Shape {
+        Shape::Circle { radius: 0.0 }
+    }
 
-struct Circle {
-    radius: f32,
-}
+    fn new_cube() -> Shape {
+        Shape::Cube { width: 0.0, height: 0.0, depth: 0.0 }
+    }
 
-struct Cube {
-    length: f32,
-    width: f32,
-    height: f32,
-}
-
-struct Sphere {
-    radius: f32,
-}
-
-impl TwoDimensional for Square {
-    fn area(&self) -> f32 {
-        self.side * self.side
+    fn new_sphere() -> Shape {
+        Shape::Sphere { radius: 0.0 }
     }
 }
 
-impl TwoDimensional for Rectangle {
+impl Shape {
     fn area(&self) -> f32 {
-        self.length * self.width
+        match self {
+            Shape::Square { side } => side * side,
+            Shape::Rectangle { length, width } => length * width,
+            Shape::Circle { radius } => std::f32::consts::PI * radius * radius,
+            Shape::Cube { width, height, depth } => {
+                // Add all area's of the cube
+                2.0 * width * height // two sides
+                    + 2.0 * width * depth // next two sides
+                    + 2.0 * height * depth // final two sides
+            }
+            Shape::Sphere { radius } => 4.0 * std::f32::consts::PI * radius * radius,
+        }
     }
-}
 
-impl TwoDimensional for Circle {
-    fn area(&self) -> f32 {
-        std::f32::consts::PI * self.radius * self.radius
-    }
-}
-
-impl ThreeDimensional for Cube {
     fn volume(&self) -> f32 {
-        self.length * self.width * self.height
-    }
-}
-
-impl ThreeDimensional for Sphere {
-    fn volume(&self) -> f32 {
-        (4.0 / 3.0) * std::f32::consts::PI * self.radius * self.radius * self.radius
-    }
-}
-
-impl TwoDimensional for Sphere {
-    fn area(&self) -> f32 {
-        4.0 * std::f32::consts::PI * self.radius * self.radius
-    }
-}
-
-impl Sphere {
-    fn increase_radius(&mut self, increase: f32) {
-        self.radius += increase;
-    }
-
-    fn decrease_radius(&mut self, decrease: f32) {
-        self.radius = self.radius - decrease;
-        if self.radius < 0.0 {
-            println!("\x1b[31mNew radius would become {}, so we changed it to 0.0\x1b[0m", self.radius);
-            self.radius = 0.0
+        match self {
+            Shape::Cube { width, height, depth } => width * height * depth,
+            Shape::Sphere { radius } => 4.0 / 3.0 * std::f32::consts::PI * radius * radius * radius,
+            _ => panic!("Not a 3D shape"),
         }
     }
 
     fn print_aspects(&self) -> () {
-        println!("Aspects of sphere: r={}m, area={}m2, volume={}m3", self.radius, self.area(), self.volume());
+        match self {
+            Shape::Square { side } => {
+                println!(
+                    "Aspects of square: s={}m, area={}m2",
+                    side,
+                    self.area()
+                );
+            }
+            Shape::Rectangle { length, width } => {
+                println!(
+                    "Aspects of rectangle: l={}m, w={}m, area={}m2",
+                    length,
+                    width,
+                    self.area()
+                );
+            }
+            Shape::Circle { radius } => {
+                println!(
+                    "Aspects of circle: r={}m, area={}m2",
+                    radius,
+                    self.area()
+                );
+            }
+            Shape::Cube { width, height, depth } => {
+                println!(
+                    "Aspects of cube: w={}m, h={}m, d={}m, area={}m2, volume={}m3",
+                    width,
+                    height,
+                    depth,
+                    self.area(),
+                    self.volume()
+                );
+            }
+            Shape::Sphere { radius } => {
+                println!(
+                    "Aspects of sphere: r={}m, area={}m2, volume={}m3",
+                    radius,
+                    self.area(),
+                    self.volume()
+                );
+            }
+        }
+    }
+}
+
+// pub fn area<T: TwoDimensional> (shape: &T) -> f32 {
+//     shape.area()
+// }
+
+
+trait HasRadius {
+    fn increase_radius(&mut self, increase: f32) -> Result<(), Error>;
+    fn decrease_radius(&mut self, decrease: f32) -> Result<(), Error>;
+}
+
+impl HasRadius for Shape {
+    fn increase_radius(&mut self, increase: f32) -> Result<(), Error> {
+        // See if the Shape type we have has attribute radius
+        match self {
+            Shape::Circle { radius } => {
+                *radius += increase;
+                Ok(())
+            }
+            Shape::Sphere { radius } => {
+                *radius += increase;
+                Ok(())
+            }
+            _ => Err(Error::new(std::io::ErrorKind::Other, "Not a shape with radius"))
+        }
+    }
+
+    fn decrease_radius(&mut self, decrease: f32) -> Result<(), Error> {
+        match self {
+            Shape::Circle { radius } => {
+                *radius = *radius - decrease;
+                if *radius < 0.0 {
+                    println!(
+                        "\x1b[31mNew radius would become {}, so we changed it to 0.0\x1b[0m",
+                        *radius
+                    );
+                    *radius = 0.0
+                }
+                Ok(())
+            }
+            Shape::Sphere { radius } => {
+                *radius = *radius - decrease;
+                if *radius < 0.0 {
+                    println!(
+                        "\x1b[31mNew radius would become {}, so we changed it to 0.0\x1b[0m",
+                        *radius
+                    );
+                    *radius = 0.0
+                }
+                Ok(())
+            }
+            _ => Err(Error::new(std::io::ErrorKind::Other, "Not a shape with radius"))
+        }
     }
 }
 
 fn main() {
     println!("== Play around with traits by using shapes");
 
-    let square = Square { side: 5.0 };
-    let rectangle = Rectangle { length: 5.0, width: 14.0 };
-    let circle = Circle { radius: 3.0 };
-    let cube = Cube { length: 2.0, width: 2.0, height: 2.0 };
-    let mut sphere = Sphere { radius: std::f32::consts::E };
+    let square = Shape::new_square();
+    let mut rectangle = Shape::new_rectangle();
+    let circle = Shape::new_circle();
+    let cube = Shape::new_cube();
+    let mut sphere = Shape::new_sphere();
 
     println!("Area of square: {}", square.area());
     println!("Area of rectangle: {}", rectangle.area());
@@ -99,9 +174,16 @@ fn main() {
     println!("Volume of sphere: {}", sphere.volume());
 
     sphere.print_aspects();
-    sphere.increase_radius(2.0 * std::f32::consts::E);
+    sphere.increase_radius(2.0 * std::f32::consts::E).expect("Increase of radius");
     sphere.print_aspects();
-    sphere.decrease_radius(std::f32::consts::LN_2);
-    sphere.decrease_radius(500.0);
+    sphere.decrease_radius(std::f32::consts::LN_2).expect("Increase of radius by ln(2)");
+    sphere.decrease_radius(500.0).expect("Decrease of radius by 500.0 (resulting in 0.0)");
     sphere.print_aspects();
+
+    // Should be empty
+    assert_eq!(sphere.volume(), 0.0);
+
+    let error_string = rectangle.increase_radius(2.0)
+        .expect_err("Should have an error");
+    println!("Error: {}", error_string);
 }
